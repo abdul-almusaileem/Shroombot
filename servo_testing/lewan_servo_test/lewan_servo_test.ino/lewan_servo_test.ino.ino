900027736b058be0d0054793ec73b03c87f19319
abdul-almusaileem <7amanga@gmail.com>
2019-03-30 21:56:24 -0400

    adding the testing stuff

---
+++ servo_testing/lewan_servo_test/lewan_servo_test.ino/lewan_servo_test.ino.ino
@@ -0,0 +1,41 @@
+#include <ServoLX.h>
+
+int rx = 8;
+int tx = 7;
+
+ServoLX servo(rx, tx);
+
+int id;
+
+void setup()
+{
+ // activate the motor serial connection
+ //
+ servo.begin();
+ Serial.begin(115200);
+
+ // initial stop the motor
+ //
+ servo.disable();
+
+}
+
+void loop()
+{
+    id = 1;
+    servo.setid(id);
+    servo.enable();
+    //int pos = servo.position_raw(id);
+	//Serial.println(pos);
+
+  //servo.position_raw(id);
+
+  servo.move(id, 10.3);
+  servo.start();
+
+  servo.move_raw(id, 500);
+  servo.start();
+
+  Serial.println("nothing yet...");
+  delay(1000);
+}
