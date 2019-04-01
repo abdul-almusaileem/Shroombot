b3a26287f9b81750e74f51b545cd9badafd43c03
abdul-almusaileem <7amanga@gmail.com>
2019-04-01 15:22:08 -0400

    still testing

--- servo_testing/chinese_lobot_servo_control/LX15D_Initial/LX15D_Initial.ino
+++ servo_testing/chinese_lobot_servo_control/LX15D_Initial/LX15D_Initial.ino
@@ -1,4 +1,4 @@
-
+#include <SoftwareSerial.h>
 
 //
 //
@@ -14,6 +14,11 @@
 #define LOBOT_SERVO_FRAME_HEADER         0x55
 #define LOBOT_SERVO_MOVE_TIME_WRITE      1
 #define LOBOT_SERVO_ID_WRITE             13
+
+int dir = 0;
+int flag = 0;
+
+SoftwareSerial motor(10, 11);
 
 byte LobotCheckSum(byte buf[])   //校验数据
 {
@@ -51,6 +56,20 @@ void LobotSerialServoSetID(HardwareSerial &SerialX, uint8_t oldID, uint8_t newID
   byte buf[7];
   buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
   buf[2] = oldID;
+  buf[3] = 11;
+  buf[4] = LOBOT_SERVO_ID_WRITE;
+  buf[5] = newID;
+  buf[6] = LobotCheckSum(buf);
+  SerialX.write(buf, 7);
+}
+
+
+
+void LobotSerialServoSetID_software(SoftwareSerial &SerialX, uint8_t oldID, uint8_t newID)    //设置舵机号
+{
+  byte buf[7];
+  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
+  buf[2] = oldID;
   buf[3] = 4;
   buf[4] = LOBOT_SERVO_ID_WRITE;
   buf[5] = newID;
@@ -61,17 +80,61 @@ void LobotSerialServoSetID(HardwareSerial &SerialX, uint8_t oldID, uint8_t newID
 void setup() {
   // put your setup code here, to run once:
   // put your setup code here, to run once:
+  //
+
+
+  byte buf[7];
+  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
+  buf[2] = 0xFE;       // id
+  buf[3] = 0x4;
+  buf[4] = 0x13;
+  buf[5] = 0x2;
+  buf[6] = LobotCheckSum(buf);
+  Serial.write(buf, 7);
+
+
+
+
   Serial.begin(115200);  //初始化波特率
   delay(1000);
-  LobotSerialServoSetID(Serial, 254, 1);   //设置所有舵机为1号舵机
-  //LobotSerialServoSetID(Serial, 254, 2);   //设置所有舵机为1号舵机
+  //LobotSerialServoSetID(Serial, 1, 2);   //设置所有舵机为1号舵机
+  //LobotSerialServoSetID_software(motor, 254, 2);   //设置所有舵机为1号舵机
   delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  LobotSerialServoMove(Serial, 1, 100, 1000);   //使1号舵机用500ms的时间转动到100的位置
-    delay(2000);  //延时，等待舵机运行
-  LobotSerialServoMove(Serial, 1, 700, 1000);   //使1号舵机用500ms的时间转动到500的位置
-    delay(2000);  //延时，等待舵机运行
+  //
+ // LobotSerialServoMove(Serial, 1, dir, 1000);   //使1号舵机用500ms的时间转动到100的位置
+//    delay(2000);  //延时，等待舵机运行
+  //LobotSerialServoMove(Serial, 1, 700, 1000);   //使1号舵机用500ms的时间转动到500的位置
+    //delay(2000);  //延时，等待舵机运行
+
+
+    byte buf[7];
+    buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
+    buf[2] = 0xFE;       // id
+    buf[3] = 0x4;
+    buf[4] = 0x13;
+    buf[5] = 0x2;
+    buf[6] = LobotCheckSum(buf);
+    Serial.write(buf, 7);
+
+
+    LobotSerialServoMove(Serial,2, 500, 1000);   //使1号舵机用500ms的时间转动到100的位置
+    delay(2000);
+
+
+
+    //LobotSerialServoMove(Serial, 2, 500, 1000);   //使1号舵机用500ms的时间转动到100的位置
+    //delay(2000);
+
+    //LobotSerialServoMove(Serial, 2, 1000, 1000);   //使1号舵机用500ms的时间转动到100的位置
+    //delay(2000);
+
+    LobotSerialServoMove(Serial, 2, 0, 1000);   //使1号舵机用500ms的时间转动到100的位置
+    delay(2000);
+
+    Serial.println("");
+    Serial.println(dir);
 }
