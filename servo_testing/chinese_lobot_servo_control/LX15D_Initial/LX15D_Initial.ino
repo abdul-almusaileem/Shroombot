e6ce563b76bd59a3d8324e82ed9dfb81e2396687
abdul-almusaileem <7amanga@gmail.com>
2019-04-03 21:00:35 -0400

    code needs more testing

--- servo_testing/chinese_lobot_servo_control/LX15D_Initial/LX15D_Initial.ino
+++ servo_testing/chinese_lobot_servo_control/LX15D_Initial/LX15D_Initial.ino
@@ -15,11 +15,92 @@
 #define LOBOT_SERVO_MOVE_TIME_WRITE      1
 #define LOBOT_SERVO_ID_WRITE             13
 
-int dir = 0;
-int flag = 0;
 
-SoftwareSerial motor(10, 11);
+// number of servos used
+//
+#define NUM_SERVOS 2
 
+
+// create the servo opjects
+//
+SoftwareSerial servo_1(5, 6);
+SoftwareSerial servo_2(7, 8);
+
+// create an array of servos
+//
+SoftwareSerial servos[NUM_SERVOS] = {servo_1, servo_2};
+
+// input values to move the servos
+//
+int16_t servo;
+int16_t position;
+
+// function prototypes
+//
+void servos_init(SoftwareSerial servos[]);
+byte LobotCheckSum(byte buf[]);
+void servo_set_id(SoftwareSerial &SerialX, uint8_t oldID, uint8_t newID);
+void servo_move(SoftwareSerial &SerialX, uint8_t id, int16_t position,
+                uint16_t time);
+
+
+//
+//
+void setup() {
+    // initial Harsware Serial for serial input
+    //
+    Serial.begin(9600);
+    delay(1000);
+
+    // initialize the servoes using the SoftwareSerial
+    //
+    servos_init(servos);
+    delay(1000);
+}
+
+void loop() {
+
+    while (Serial.available()) {
+        // read which servo and what position to move it
+        //
+        servo = Serial.parseInt();
+        position = Serial.parseInt();
+
+        // move the picked servoe to the specified position
+        //
+        switch (servo) {
+            case 1:
+                Serial.print("first motor -> ");
+                Serial.println(position);
+                servo_move(servo_1, 1, position, 1000);
+                break;
+            case 2:
+                Serial.print("second motor -> ");
+                Serial.println(position);
+                servo_move(servo_2, 1, position, 1000);
+                break;
+            default:
+                Serial.println("Undeclared Servo");
+                continue;
+        }
+        delay(2000);
+    }
+}
+
+
+// initialize all the servos that wehave
+//
+void servos_init(SoftwareSerial servo[]) {
+    for (int i = 0; i < NUM_SERVOS; i++) {
+        servos[i].begin(115200);
+        servo_set_id(servos[i], 254, 1);
+        Serial.println(i);
+    }
+}
+
+// checksum
+// TODO: HOW DOES IT WORK
+//
 byte LobotCheckSum(byte buf[])   //校验数据
 {
   byte i;
@@ -32,7 +113,49 @@ byte LobotCheckSum(byte buf[])   //校验数据
   return i;
 }
 
-void LobotSerialServoMove(HardwareSerial &SerialX, uint8_t id, int16_t position, uint16_t time)  //设置舵机转动
+//
+//
+void servo_set_id(SoftwareSerial &SerialX, uint8_t oldID, uint8_t newID) {
+  byte buf[7];
+  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
+  buf[2] = oldID;
+  buf[3] = 4;
+  buf[4] = LOBOT_SERVO_ID_WRITE;
+  buf[5] = newID;
+  buf[6] = LobotCheckSum(buf);
+  SerialX.write(buf, 7);
+}
+
+//
+//
+void servo_move(SoftwareSerial &SerialX, uint8_t id, int16_t position,
+                uint16_t time)  {
+  byte buf[10];
+  if(position < 0)
+    position = 0;
+  if(position > 1000)
+    position = 1000;
+  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
+  buf[2] = id;
+  buf[3] = 7;
+  buf[4] = LOBOT_SERVO_MOVE_TIME_WRITE;
+  buf[5] = GET_LOW_BYTE(position);
+  buf[6] = GET_HIGH_BYTE(position);
+  buf[7] = GET_LOW_BYTE(time);
+  buf[8] = GET_HIGH_BYTE(time);
+  buf[9] = LobotCheckSum(buf);
+  SerialX.write(buf, 10);
+}
+
+
+/*
+// hardware serial
+//
+
+//
+//
+void LobotSerialServoMove(HardwareSerial &SerialX, uint8_t id, int16_t position,
+                         uint16_t time)  //设置舵机转动
 {
   byte buf[10];
   if(position < 0)
@@ -51,21 +174,9 @@ void LobotSerialServoMove(HardwareSerial &SerialX, uint8_t id, int16_t position,
   SerialX.write(buf, 10);
 }
 
-void LobotSerialServoSetID(HardwareSerial &SerialX, uint8_t oldID, uint8_t newID)    //设置舵机号
-{
-  byte buf[7];
-  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-  buf[2] = oldID;
-  buf[3] = 11;
-  buf[4] = LOBOT_SERVO_ID_WRITE;
-  buf[5] = newID;
-  buf[6] = LobotCheckSum(buf);
-  SerialX.write(buf, 7);
-}
-
-
-
-void LobotSerialServoSetID_software(SoftwareSerial &SerialX, uint8_t oldID, uint8_t newID)    //设置舵机号
+//
+//
+void LobotSerialServoSetID(HardwareSerial &SerialX, uint8_t oldID, uint8_t newID)
 {
   byte buf[7];
   buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
@@ -76,65 +187,4 @@ void LobotSerialServoSetID_software(SoftwareSerial &SerialX, uint8_t oldID, uint
   buf[6] = LobotCheckSum(buf);
   SerialX.write(buf, 7);
 }
-
-void setup() {
-  // put your setup code here, to run once:
-  // put your setup code here, to run once:
-  //
-
-
-  byte buf[7];
-  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-  buf[2] = 0xFE;       // id
-  buf[3] = 0x4;
-  buf[4] = 0x13;
-  buf[5] = 0x2;
-  buf[6] = LobotCheckSum(buf);
-  Serial.write(buf, 7);
-
-
-
-
-  Serial.begin(115200);  //初始化波特率
-  delay(1000);
-  //LobotSerialServoSetID(Serial, 1, 2);   //设置所有舵机为1号舵机
-  //LobotSerialServoSetID_software(motor, 254, 2);   //设置所有舵机为1号舵机
-  delay(1000);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  //
- // LobotSerialServoMove(Serial, 1, dir, 1000);   //使1号舵机用500ms的时间转动到100的位置
-//    delay(2000);  //延时，等待舵机运行
-  //LobotSerialServoMove(Serial, 1, 700, 1000);   //使1号舵机用500ms的时间转动到500的位置
-    //delay(2000);  //延时，等待舵机运行
-
-
-    byte buf[7];
-    buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
-    buf[2] = 0xFE;       // id
-    buf[3] = 0x4;
-    buf[4] = 0x13;
-    buf[5] = 0x2;
-    buf[6] = LobotCheckSum(buf);
-    Serial.write(buf, 7);
-
-
-    LobotSerialServoMove(Serial,2, 500, 1000);   //使1号舵机用500ms的时间转动到100的位置
-    delay(2000);
-
-
-
-    //LobotSerialServoMove(Serial, 2, 500, 1000);   //使1号舵机用500ms的时间转动到100的位置
-    //delay(2000);
-
-    //LobotSerialServoMove(Serial, 2, 1000, 1000);   //使1号舵机用500ms的时间转动到100的位置
-    //delay(2000);
-
-    LobotSerialServoMove(Serial, 2, 0, 1000);   //使1号舵机用500ms的时间转动到100的位置
-    delay(2000);
-
-    Serial.println("");
-    Serial.println(dir);
-}
+*/
--- servo_testing/lewan_servo_test/lewan_servo_test.ino/lewan_servo_test.ino.ino
+++ servo_testing/lewan_servo_test/lewan_servo_test.ino/lewan_servo_test.ino.ino
@@ -1,42 +1,198 @@
-#include <ServoLX.h>
 
-int rx = 8;
-int tx = 7;
 
-ServoLX servo(rx, tx);
 
-int id;
+/*
+    mid_points = {  1: 450,
+                    2: 450,
+                    3: 675,
+                    4: 275
+                }
+*/
 
-void setup()
-{
- // activate the motor serial connection
- //
- //
- servo.begin();
- Serial.begin(115200);
+// TODO: 1) write the begin & and the idle functions.
+// TODO: 2) send the values at once and maybe move at once too.
+// TODO: 3) control with some sort of controller.
+// TODO: 4)
+//
 
- // initial stop the motor
- //
- servo.disable();
+
+//
+//
+#include <SoftwareSerial.h>
+// split the 16bit to two 8bit cars concat the the upper and lower bits
+//
+#define GET_LOW_BYTE(A) (uint8_t)((A))
+#define GET_HIGH_BYTE(A) (uint8_t)((A) >> 8)
+#define BYTE_TO_HW(A, B) ((((uint16_t)(A)) << 8) | (uint8_t)(B))
+#define LOBOT_SERVO_FRAME_HEADER         0x55
+#define LOBOT_SERVO_MOVE_TIME_WRITE      1
+#define LOBOT_SERVO_ID_WRITE             13
+
+
+// number of servos used
+//
+#define NUM_SERVOS 4
+
+
+// creatte the SoftwareSerial objects
+// SoftwareSerial serial(Tx, Rx)
+// we are using the Rx pins
+//
+SoftwareSerial servo_1(1, 2);
+SoftwareSerial servo_2(3, 4);
+SoftwareSerial servo_3(5, 6);
+SoftwareSerial servo_4(7, 8);
+
+// create an array of servos
+//
+SoftwareSerial servos[NUM_SERVOS] = {servo_1, servo_2, servo_3, servo_4};
+
+// input values to move the servos
+//
+int16_t servo;
+int16_t position;
+
+// function prototypes
+//
+void servos_begin(SoftwareSerial servos[]);
+void servos_idle(SoftwareSerial servos[]);
+void servo_set_id(SoftwareSerial &SerialX, uint8_t oldID, uint8_t newID);
+void servo_move(SoftwareSerial &SerialX, uint8_t id, int16_t position,
+                uint16_t time);
+byte LobotCheckSum(byte buf[]);
+
+
+//
+//
+void setup() {
+    // initial Harsware Serial for serial input
+    //
+    Serial.begin(9600);
+    delay(1000);
+
+    // TODO: delete this and just use the servos_begin
+    //
+    servo_1.begin(115200);
+    servo_2.begin(115200);
+    servo_3.begin(115200);
+    servo_4.begin(115200);
+    servo_set_id(servo_1, 254, 1);
+    servo_set_id(servo_2, 254, 1);
+    servo_set_id(servo_3, 254, 1);
+    servo_set_id(servo_4, 254, 1);
+
+
+    // initialize the servoes using the SoftwareSerial
+    // FIXME: Try to make it work to look neat...
+    //
+    //servos_begin(servos);
+}
+
+void loop() {
+
+    while (Serial.available()) {
+        // read which servo and what position to move it
+        //
+        servo = Serial.parseInt();
+        position = Serial.parseInt();
+
+        // move the picked servoe to the specified position
+        //
+        switch (servo) {
+            case 1:
+                Serial.print("first motor -> ");
+                Serial.println(position);
+                servo_move(servo_1, 1, position, 1000);
+                break;
+            case 2:
+                Serial.print("second motor -> ");
+                Serial.println(position);
+                servo_move(servo_2, 1, position, 1000);
+                break;
+                case 3:
+                Serial.print("Third motor -> ");
+                Serial.println(position);
+                servo_move(servo_3, 1, position, 1000);
+                break;
+
+            case 4:
+                Serial.print("Fourth motor -> ");
+                Serial.println(position);
+                servo_move(servo_4, 1, position, 1000);
+                break;
+            default:
+                Serial.println("Undeclared Servo");
+                continue;
+        }
+        delay(2000);
+    }
+}
+
+
+// initialize all the servos that wehave
+// TODO: add led to announce the that the init finished 
+//
+void servos_begin(SoftwareSerial servo[]) {
+    for (int i = 0; i < NUM_SERVOS-1; i++) {
+        servo[i].begin(115200);
+        servo_set_id(servo[i], 254, 1);
+        delay(1000);
+    }
+}
+
+// puts the arm into IDLE position
+// TODO: make it work!!
+void servos_idle(SoftwareSerial servos[]) {
 
 }
 
-void loop()
-{
-    id = 1;
-    servo.setid(id);
-    servo.enable();
-    //int pos = servo.position_raw(id);
-	//Serial.println(pos);
+// set the servo's ID
+// TODO: HOW DOES IT WORK
+//
+void servo_set_id(SoftwareSerial &SerialX, uint8_t oldID, uint8_t newID) {
+  byte buf[7];
+  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
+  buf[2] = oldID;
+  buf[3] = 4;
+  buf[4] = LOBOT_SERVO_ID_WRITE;
+  buf[5] = newID;
+  buf[6] = LobotCheckSum(buf);
+  SerialX.write(buf, 7);
+}
 
-  //servo.position_raw(id);
+// move the servo to a specified position
+// TODO: HOW DOES IT WORK
+// TODO: play with time var
+//
+void servo_move(SoftwareSerial &SerialX, uint8_t id, int16_t position,
+                uint16_t time)  {
+  byte buf[10];
+  if(position < 0)
+    position = 0;
+  if(position > 1000)
+    position = 1000;
+  buf[0] = buf[1] = LOBOT_SERVO_FRAME_HEADER;
+  buf[2] = id;
+  buf[3] = 7;
+  buf[4] = LOBOT_SERVO_MOVE_TIME_WRITE;
+  buf[5] = GET_LOW_BYTE(position);
+  buf[6] = GET_HIGH_BYTE(position);
+  buf[7] = GET_LOW_BYTE(time);
+  buf[8] = GET_HIGH_BYTE(time);
+  buf[9] = LobotCheckSum(buf);
+  SerialX.write(buf, 10);
+}
 
-  servo.move(id, 10.3);
-  servo.start();
-
-  servo.move_raw(id, 500);
-  servo.start();
-
-  Serial.println("nothing yet...");
-  delay(1000);
+// checksum to make sure that the packet was sent
+// TODO: HOW DOES IT WORK
+//
+byte LobotCheckSum(byte buf[]) {
+  byte i;
+  uint16_t temp = 0;
+  for (i = 2; i < buf[3] + 2; i++) {
+    temp += buf[i];
+  }
+  temp = ~temp;
+  i = (byte)temp;
+  return i;
 }
