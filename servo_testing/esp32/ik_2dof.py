db0d03e1d5aea0eebf014fede568b3aaaff84c2d
abdul-almusaileem <7amanga@gmail.com>
2019-09-08 14:42:29 -0400

    this works maybe clean but because we're working on 3 dof no need?

--- servo_testing/esp32/ik_2dof.py
+++ servo_testing/esp32/ik_2dof.py
@@ -8,8 +8,8 @@ a2 = 3
 
 # the desired point 
 # 
-x = int(input("enter x: "))
-y = int(input("enter y: "))
+x = float(input("enter x: "))
+y = float(input("enter y: "))
 
 
 #
@@ -29,8 +29,42 @@ phi3 = acos((r**2 - a2**2 - a1**2) / (-2 * a2 * a1))
 theta2 = 180 - rad2deg(phi3)
 theta2_deg = rad2deg(theta2)
 
-print("theta1: {}".format(theta1_deg))
+# TODO: clean this make one loop or no loop do mAtH maybe
+#
+while(theta2_deg  > 360 or theta1_deg  > 360):
+    if(theta2_deg > 360):
+        theta2_deg -=  360
+    elif(theta1_deg > 360):
+        theta1_deg -= 360
+    
+
+
+
+# while(theta2_deg < 0 or theta1_deg  < 0):
+#     if(theta2_deg < 0):
+#         theta2_deg +=  360
+#     elif(theta1_deg < 0):
+#         theta1_deg += 360
+
+
+print("theta1: {}".format(theta1_deg ))
 print("theta2: {}".format(theta2_deg)) 
 
-print("first servo put: {}".format(theta1_deg * 0.24))
-print("second servo put: {}".format(theta2_deg * 0.24))
+
+# add some stuff to only on the last quarter 
+#
+servo1_pos = abs(theta1_deg) / 0.24
+servo2_pos = abs(theta2_deg) / 0.24 
+
+if(servo1_pos > 1000):
+    servo1_pos -= 500
+if(servo2_pos > 1000):
+    servo2_pos -= 500
+    
+if(servo1_pos < 500):
+    servo1_pos += 500
+if(servo2_pos < 500):
+    servo2_pos += 500
+
+print("first servo put: {}".format(int(servo1_pos)))
+print("second servo put: {}".format(int(servo2_pos)))
