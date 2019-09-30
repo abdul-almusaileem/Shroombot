a8cdb7f6f8a1a14ade64c0c2dac30f068c778769
abdul-almusaileem <7amanga@gmail.com>
2019-09-30 15:44:13 -0400

    arm test i think it works!!!

--- arm/arm_graph_simulation/arm_test.py
+++ arm/arm_graph_simulation/arm_test.py
@@ -3,6 +3,8 @@ from ikpy.chain import Chain
 from ikpy.link import OriginLink, URDFLink
 from ikpy import plot_utils
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 # TODO: change translation vectors to vars of lengths
 #
@@ -35,25 +37,26 @@ arm = Chain(name="arm", links= [
 
     URDFLink(
         name = "wrist",
-        translation_vector = [0.2, 0, 0], # location
+        translation_vector = [0.25, 0, 0], # location
         orientation = [0, 0, 0],
         rotation = [0, 1, 0])
 ])
 
 # the target point between -1 ~ 1 in all axis 
 # 
-target_vector = [.5, -0.75, 0]
+target_vector = [.5, .25, .30]
 target_frame = np.eye(4)
 target_frame[:3, 3] = target_vector
 
 
-print(my_chain)
+print(arm)
+print("The angles of each joints are : ", arm.inverse_kinematics(target_frame))
 
-real_frame = my_chain.forward_kinematics(my_chain.inverse_kinematics(target_frame))
+real_frame = arm.forward_kinematics(arm.inverse_kinematics(target_frame))
 print("Computed position vector : %s, original position vector : %s" % (real_frame[:3, 3], target_frame[:3, 3]))
 
 ax = plot_utils.init_3d_figure()
-my_chain.plot(my_chain.inverse_kinematics(target_frame), ax, target=target_vector)
+arm.plot(arm.inverse_kinematics(target_frame), ax, target=target_vector)
 plt.xlim(-1, 1)
 plt.ylim(-1, 1)
 plt.show()
