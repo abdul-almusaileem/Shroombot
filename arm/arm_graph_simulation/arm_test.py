e22c0f53701212f7b887e93efc17fbc026a3204d
abdul-almusaileem <7amanga@gmail.com>
2019-10-03 19:59:28 -0400

    added boundes for each links need to check bounds for upper elbow

--- arm/arm_graph_simulation/arm_test.py
+++ arm/arm_graph_simulation/arm_test.py
@@ -8,7 +8,7 @@ from time import sleep
 import math
 
 
-SCALER = 1
+SCALER = 10
 
 # TODO: change translation vectors to vars of lengths
 # TODO: change the translation vectors to be not on one axis because that
@@ -21,32 +21,50 @@ arm = Chain(name="arm", links= [
         name = "base servo",
         translation_vector = [0, 0, 0], # location 
         orientation = [0, 0, 0],
-        rotation = [0, 0, 1]),
+        rotation = [0, 0, 1],
+        bounds = (math.radians(0), math.radians(192))
+        # bounds = (math.radians(-192), math.radians(0))
+        ),
     
     URDFLink(
         name = "elbow low",
         translation_vector = [0, 0, 0.85 * SCALER], # location
         orientation = [0, 0, 0],
-        rotation = [0, 1, 0]),
+        rotation = [0, 1, 0],
+        bounds = (math.radians(36), math.radians(144))
+        # bounds = (math.radians(-144), math.radians(36))
+
+        ),
     
     URDFLink(
         name = "elbow hight",
         translation_vector = [0.25 * SCALER, 0, 0], # location
         orientation = [0, 0, 0],
-        rotation = [0, 1, 0]),
+        rotation = [0, 1, 0],
+        bounds = (math.radians(0), math.radians(28)) # FIXME: CHECK BOUNDS
+        # bounds = (math.radians(-28), math.radians(0))
+
+        ),
 
     URDFLink(
         name = "middle",
         translation_vector = [0.9 * SCALER, 0, 0], # location
-        orientation = [1, 0, 0],
-        rotation = [0, 1, 0]),
+        orientation = [0, 0, 0],
+        rotation = [0, 1, 0],
+        bounds = (math.radians(144), math.radians(240))
+        # bounds = (math.radians(-144), math.radians(240))
+        
+        ),
 
     URDFLink(
         name = "wrist",
         translation_vector = [0.55 * SCALER, 0, 0], # location
         orientation = [0, 0, 0],
-        rotation = [0, 1, 0])
-    
+        rotation = [0, 1, 0],
+        bounds = (math.radians(120), math.radians(216))
+        # bounds = (math.radians(-120), math.radians(216))
+        )
+     
     # ,URDFLink(
     #     name = "end effector",
     #     translation_vector = [0.1*10, 0, 0], # location
@@ -63,26 +81,24 @@ ax = plot_utils.init_3d_figure()
 # the target point between -1 ~ 1 in all axis 
 # create the 
 #
-x = 0 * SCALER
-y = 0* SCALER
-z = 10 
+x = .0 * SCALER
+y = 3. * SCALER
+z = .0 * SCALER
 target_vector = [x, y, z]
 target_frame = np.eye(4)
 target_frame[:3, 3] = target_vector
 
 
-# print(arm)
 angles = arm.inverse_kinematics(target_frame)
 for (i, angle) in enumerate(angles):
-    angles[i] = math.degrees(angle)
+    angles[i] = math.degrees(angle) 
+    if (angles[i] > 360 ):
+        angles[i] = angles[i] % 360
+        
     print("angle({}) = {} deg, {} pos".format(i, angles[i], int(angles[i]/0.24)))
-# print("The angles of each joints are : ", angles )
 
 
 arm.plot(arm.inverse_kinematics(target_frame), ax, target=target_vector)
-# plt.xlim(-1, 1)
-# plt.ylim(-1, 1)
-# plt.show()
 
 
 # sleep(2)
@@ -120,8 +136,8 @@ tmp_target_vector = target_vector
 
 # plot 
 #
-plt.xlim(-SCALER, SCALER)
-plt.ylim(-SCALER, SCALER)
+# plt.xlim(-SCALER, SCALER)
+# plt.ylim(-SCALER, SCALER)
 plt.show()
 
 real_frame = arm.forward_kinematics(arm.inverse_kinematics(target_frame))
