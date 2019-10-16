d659d79849601795f712fbaee3f2810d14634f5e
abdul-almusaileem <7amanga@gmail.com>
2019-10-16 10:36:34 -0400

    first try: wrote the module, no test yet

---
+++ arm/rpi/arm_chain.py
@@ -0,0 +1,52 @@
+
+
+from ikpy.chain import Chain
+from ikpy.link import OriginLink, URDFLink
+from ikpy import plot_utils
+import math
+
+
+# create the arm using IKpy's chain constructor with URDF links
+# TODO: check length of each link could be wrong
+#
+arm = Chain(name="arm", links= [
+    URDFLink(
+        name = "base servo",
+        translation_vector = [0, 0, 0],
+        orientation = [ 0, 0, 0],
+        rotation = [0, 0, 1]
+        ),
+    URDFLink(
+        name = "elbow low",
+        translation_vector = [0, 0, 9.5],
+        orientation = [0, 0, 0],
+        rotation = [0, 1, 0],
+        bounds = (math.radians(0), math.radians(240))
+        ),
+    URDFLink(
+        name = "elbow hight",
+        translation_vector = [0, 0, 2.5],
+        orientation = [0, 0, 0],
+        rotation = [0, 1, 0]
+        ),
+
+    URDFLink(
+        name = "middle",
+        translation_vector = [9, 0, 0],
+        orientation = [0, 0, 0],
+        rotation = [0, 1, 0] 
+        ),
+
+    URDFLink(
+        name = "wrist",
+        translation_vector = [5.5, 0, 0],
+        orientation = [0, 0, 0],
+        rotation = [0, 1, 0]
+        )
+     
+    # ,URDFLink(
+    #     name = "end effector",
+    #     translation_vector = [1, 0, 0],
+    #     orientation = [0, 0, 0],
+    #     rotation = [0, 1, 0])
+])
