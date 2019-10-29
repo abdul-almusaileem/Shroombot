ddb5ee206b8191786864ae6729288b550ea42275
abdul-almusaileem <7amanga@gmail.com>
2019-10-29 16:28:02 -0400

    got the point down working

--- arm/rpi/remap.py
+++ arm/rpi/remap.py
@@ -1,23 +1,24 @@
 
+import math
+
 # this function is to remap the coordinates using Brandon's lOgIc
 #
-def remap(id=0, angles=[], negX_flag=0):
-        
+def remap(id=0, angles=[]):#, scan_flag=1):
+
     # reference angles for Brandon's LoGic
     # TODO: CLEAN OR FIND A WAY TO EXPLAIN
     #
     REF_ANGLES = [55, 120, 36, 120, 120, 120]
 
+    # remap by id
+    #    
     if(id == 0):
         angles[id] = REF_ANGLES[id] + angles[id]
-    # angles[id] = angles[id] + REF_ANGLES[id]
-    if(id != 0 and angles[id] >= 0):
+    elif (id == 4):
+        angles[id] = REF_ANGLES[id] - (90 + -1 * (angles[1] + angles[2]+ angles[3]))
+    elif(angles[id] >= 0):
         angles[id] = REF_ANGLES[id] - abs(angles[id])
-    elif(id != 0 and angles[id] < 0):
-        angles[id] = REF_ANGLES[id] + abs(angles[id])
-        
-        
-    # if (angles[id] < 0):
-    #     angles[id] = 0
-    
+    elif(angles[id] < 0):
+            angles[id] = REF_ANGLES[id] + abs(angles[id])
+
     return abs(angles[id])
