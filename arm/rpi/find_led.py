6703fdf3d6e5a768a42561066528f7a18f587823
abdul-almusaileem <7amanga@gmail.com>
2019-11-02 12:59:29 -0400

    simple script to find which gpio the led using

---
+++ arm/rpi/find_led.py
@@ -0,0 +1,15 @@
+
+# this is a simple script to find which gpio is the led using
+#
+import RPi.GPIO as GPIO 
+from time import sleep 
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+
+for i in range(1, 26):
+    GPIO.setup(i, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.output(8, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(8, GPIO.LOW) 
+    sleep(1)
