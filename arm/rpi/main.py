e2d231aa7aee1c7d6c4e0860ceea5e103f1fa728
abdul-almusaileem <7amanga@gmail.com>
2019-11-14 10:03:35 -0500

    reads from text file then pass points to the arm

--- arm/rpi/main.py
+++ arm/rpi/main.py
@@ -1,32 +1,52 @@
 
 import sys
+import time
 from move_arm import move_arm
 # 
 #
 def main():
   
-    while True:
-        try:
-            # take the coordinate of the targer
-            #
-            x_input = float(input("X: "))
-            y_input = float(input("Y: "))
 
-            # ip of esp and port for the socket connection
-            #
-            ip = "10.42.0.102"
-            port = 5001
-            
-            # 
-            #
-            move_arm(x = x_input, y = y_input, ip = ip, port = port)
+    try:
+        # ip of esp and port for the socket connection
+        #
+        ESP_IP = "10.42.0.102"
+        PORT = 5001
+        FILE_NAME = "shroom_coordinates.txt"
+        SHROOMS = []
         
-        except KeyboardInterrupt:
-            sys.exit()
-        except ValueError:
-            print("please enter a number")
-            continue 
+        # take the coordinate of the targer
+        #
+        # x_input = float(input("X: "))
+        # y_input = float(input("Y: "))
+            
+        # move the arm to the given coordinate
+        #
+        # move_arm(x = x_input, y = y_input, ip = ESP_IP, port = PORT)
 
+        # read the lines from the file
+        # remove '\n' from each line then split the into two array
+        # convert the coordinates into floats
+        # pass the coordinates to the arm  
+        #
+        with open(FILE_NAME, "r") as file:
+            SHROOMS = file.readlines()
+            
+            for shroom_posission in SHROOMS:
+                shroom_posission = shroom_posission.rstrip()
+                shroom_posission = shroom_posission.split(" ")
+
+                x = float(shroom_posission[0])
+                y = float(shroom_posission[1])
+                flag = move_arm(x=x, y=y, ip=ESP_IP, port=PORT)
+                
+                time.sleep(1)
+            
+    
+    except KeyboardInterrupt:
+        sys.exit()
+
+      
 
 #
 #
